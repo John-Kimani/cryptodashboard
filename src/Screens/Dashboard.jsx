e5589854e46bd05d74d8d383c3f8e3afcb2cf5bf
John-Kimani <kimwaweru100@gmail.com>
2023-01-25 17:09:09 +0300

    Create and position dashboard elements

--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './dashboard.css';
+import NavbarComponent from '../Components/Nav/Navbar';
+import Sidebar from '../Components/Sidebar/Sidebar';
+import ConnectWallet from '../Components/ConnectWallet/ConnectWallet';
+import Community from '../Components/Community/Community';
+import Chart from '../Components/Chart/Chart';
+import TokenTable from '../Components/TokenTable/TokenTable';
+
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <main className="Dashboard">
+        <NavbarComponent />
+        <section className="components">
+          <div className="row">
+            <div className="col-md-1 col-lg-1">
+              <Sidebar />
+            </div>
+
+            <div className="col-md-4 col-lg-4">
+              <ConnectWallet />
+              <div className="row mt-5 mb-5"></div>
+              <Community />
+            </div>
+
+            <div className="col-md-6 col-lg-6">
+              <Chart />
+              <div className="row mt-3 mb-3"></div>
+              <TokenTable />
+            </div>
+          </div>
+        </section>
+    </main>
   )
 }
 
